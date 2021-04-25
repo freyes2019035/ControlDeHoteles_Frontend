e7ffaad4ac16baa9d450de1e741807d4246e7cad
freyes2019035 <Daniel_reyes2003@outlook.com>
2021-04-24 21:37:42 -0600

    adding home and layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { LayoutComponent } from './layout/components/layout/layout.component'
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(home =>  home.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component'
+import { NavComponent } from './components/nav/nav.component'
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    CommonModule
+  ], 
+  exports: [
+    FooterComponent,
+    NavComponent
+  ]
+})
+export class SharedModule { }
