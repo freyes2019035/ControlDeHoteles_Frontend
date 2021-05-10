8c4982e5163b2f6bf8fa0d315672b8f3ee2f75df
freyes2019035 <Daniel_reyes2003@outlook.com>
2021-05-10 14:55:49 -0600

    nav of admins checking rol before links are created

--- src/app/admin/components/admin-nav/admin-nav.component.ts
+++ src/app/admin/components/admin-nav/admin-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { GlobalService } from 'src/app/core/services/global.service';
 
 @Component({
   selector: 'app-admin-nav',
@@ -15,7 +16,23 @@ export class AdminNavComponent {
       map(result => result.matches),
       shareReplay()
     );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  isAdminAPP: Boolean;
+  isHotelAdmin: Boolean;
+  isDifferent: Boolean;
+  constructor(private breakpointObserver: BreakpointObserver, private globalService: GlobalService) {
+    this.checkRol();
+  }
+  checkRol(){
+    let identity = this.globalService.getIdentity();
+    identity.user.rol == "ROL_ADMINAPP" 
+    ? 
+    this.isAdminAPP = true
+    : 
+    identity.user.rol == "ROL_ADMINHOTEL" 
+    ?
+    this.isHotelAdmin = true
+    : 
+    this.isDifferent = true;
+  }
+  
 }
